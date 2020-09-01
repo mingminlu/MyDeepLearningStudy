a9aff0a8baced7c0f8ae2267a7d1a37d088c6825
Billy Lu <billy.lu@sap.com>
2020-09-02 00:56:37 +0800

    move weight and bias inside function

--- AndGate.py
+++ AndGate.py
@@ -1,14 +1,16 @@
 import numpy as np
 
 
-def AND(x1, x2, w1, w2, b):
+def andGate(x1, x2):
     x = np.array([x1, x2])
-    w = np.array([w1, w2])
+    w = np.array([0.7, 0.5])
+    b = -0.5
     y = np.sum(w*x) + b
+    print(y)
     if y <= 0:
         return 0
     else:
         return 1
 
 
-print(AND(1,1,0.5,0.5,-0.7))
+print(andGate(1,1))
